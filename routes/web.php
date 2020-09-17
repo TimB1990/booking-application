e8a0b08532ba13316c25b21a8627d560069b1871
= <t.boons@live.nl>
2020-09-18 00:05:56 +0200

    further setup

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,17 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
+| 
 |
 */
 
 Route::get('/', function () {
+
+    /*if(Auth::check()){
+        return redirect()->route('dashboard')
+    }
+    
+    return redirect()->route('login')*/
+
     return view('welcome');
 });
