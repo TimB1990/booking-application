c91ed34d79dbf3f66daf431494c164d0ee59825a
= <t.boons@live.nl>
2020-09-19 20:09:51 +0200

    fixed responsiveness

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,10 +13,10 @@
     <title>Ressys</title>
 </head>
 <body>
-    <div class="container-fluid d-flex flex-column" style="height:100vh">
+    <div class="container-fluid">
         <!-- header -->
-        <header class="row flex-column">
-            <div class="col bg-dark" style="display:flex;justify-content:space-between;height:12.5vh">
+        <header class="row d-flex flex-column">
+            <div class="col bg-dark" style="display:flex;justify-content:space-between;padding:1rem;">
                 <h2 class="logo-text"> ® RESSYS - dashboard</h2>
 
                 <!-- div to display profile photo -->
@@ -29,21 +29,20 @@
         </header>
 
         <!-- main -->
-        <main class="row flex-grow-1">
-            <aside class="col-sm-12 col-md-12 col-lg-3 bg-dark">
-                @include('includes.side')
-            </aside>
-            <section class="col-sm-12 col-md-12 col-lg-9 bg-secondary">
-                @yield('content')
+        <main class="row" style="min-height:100vh;">
+                <aside class="side col-sm-12 col-md-12 col-lg-3 bg-dark">
+                    @include('includes.side')
+                </aside>
+                <section class="content col-sm-12 col-md-12 col-lg-9 bg-secondary">
+                    @yield('content')
+                </section> 
             </section>
         </main>
-
-        <!-- footer -->
-        <footer class="row">
-            <div class="col-12 footer bg-dark">
-                @include('includes.footer')
-            </div>
-        </footer>
     </div>
+    <footer>
+        <div class="col-12 footer bg-dark">
+            @include('includes.footer')
+        </div>
+    </footer>
 </body>
 </html>
