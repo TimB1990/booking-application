288d9caf8f3899ca2dce64420c62725d5aa52a3b
= <t.boons@live.nl>
2020-09-26 01:08:22 +0200

    loginform draft

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -8,9 +8,45 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Login</title>
 </head>
-<body>
-    <div class="login-container">
-        <code>here goes login form</code>
+<body style="border:none;">
+    <div class="login">
+        <div class="login-container">
+            <!-- header -->
+            <div class="login-container-header">
+                <p>&reg; Ressys - login</p>
+            </div>
+
+            <!-- error -->
+            @if($errors->any())
+                <div class="error">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            <!-- login form -->
+            <form class="login-container-form" action="#" method="POST">
+                @csrf
+                <p>Login as employee:</p>
+                <div class="input-container">
+                    <i class="fa fa-user"></i>
+                    <input type="text" id="name" required tabindex="1" autofocus>
+                </div>
+                <div class="input-container">
+                    <i class="fa fa-lock"></i>
+                    <input type="password" id="pass" required tabindex="2">
+                </div>
+
+                <div class="form-submit">
+                    <input type="submit"></button>
+                    <a href="#">I forgot my password</a>
+                </div>
+
+            </form>
+        </div>
     </div>
 </body>
 </html>
