f283aae508e28dede9337ebd66620c965857d292
= <t.boons@live.nl>
2020-09-25 15:13:11 +0200

    responsive fixed mainly

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -6,4 +6,4 @@
         <a href="#">Logout</a>
     </li>
 </ul>
-<span onclick="toggle('menu-grid')" id="menu-toggle"><i style="font-size:36px;" class="fa fa-th"></i></span>
+<span onclick="toggle('menu-grid')" id="menu-toggle"><i style="font-size:24px;" class="fa fa-th"></i></span>
